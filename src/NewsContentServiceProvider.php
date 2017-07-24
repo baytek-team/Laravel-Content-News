5c5fac8104df8f8852a437adba1a8a1443b21409
Sarah Marinoff <sarah@baytek.ca>
2017-07-24 17:25:44 -0400

    fixed directory case

--- src/NewsContentServiceProvider.php
+++ src/NewsContentServiceProvider.php
@@ -81,7 +81,7 @@ class NewsContentServiceProvider extends AuthServiceProvider
         });
 
         //Register factories
-        $this->app->make('Illuminate\Database\Eloquent\Factory')->load(__DIR__ . '/../database/Factories');
+        $this->app->make('Illuminate\Database\Eloquent\Factory')->load(__DIR__ . '/../database/factories');
 
     }
 
