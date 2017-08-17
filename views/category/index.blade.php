ab34c585d493e6a860b726d831685300d1bb7e42
Yvon Viger <yvon@baytek.ca>
2017-08-17 09:48:39 -0400

    Addressing the issue of no results

--- views/category/index.blade.php
+++ views/category/index.blade.php
@@ -15,46 +15,60 @@
     </div>
 @endsection
 
-@section('content')
-    <table class="ui selectable very basic table">
-        <thead>
-            <tr>
-                <th>{{ ___('Category Name') }}</th>
-                <th class="center aligned collapsing">{{ ___('Actions') }}</th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse($categories as $category)
-                <tr data-category-id="{{ $category->id }}">
-                    <td>{{ $category->title }}</td>
-                    <td class="right aligned collapsing">
-                        <div class="ui compact text menu">
-                            <a class="item" href="{{ route('news.category.edit', $category->id) }}">
-                                <i class="pencil icon"></i>
-                                {{-- {{ ___('Edit') }} --}}
-                            </a>
-                            @button('', [
-                                'method' => 'delete',
-                                'location' => 'news.category.destroy',
-                                'type' => 'route',
-                                'confirm' => 'Are you sure you want to delete this news category?</br>This cannot be undone.',
-                                'class' => 'item',
-                                'prepend' => '<i class="delete icon"></i>',
-                                'model' => $category,
-                            ])
-                        </div>
-                    </td>
-                </tr>
-            @empty
+@if(count($categories))
+    @section('content')
+        <table class="ui selectable very basic table">
+            <thead>
                 <tr>
-                    <td colspan="3">
-                        <div class="ui centered">{{ ___('There are no results') }}</div>
-                    </td>
+                    <th>{{ ___('Category Name') }}</th>
+                    <th class="center aligned collapsing">{{ ___('Actions') }}</th>
                 </tr>
-            @endforelse
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @forelse($categories as $category)
+                    <tr data-category-id="{{ $category->id }}">
+                        <td>{{ $category->title }}</td>
+                        <td class="right aligned collapsing">
+                            <div class="ui compact text menu">
+                                <a class="item" href="{{ route('news.category.edit', $category->id) }}">
+                                    <i class="pencil icon"></i>
+                                    {{-- {{ ___('Edit') }} --}}
+                                </a>
+                                @button('', [
+                                    'method' => 'delete',
+                                    'location' => 'news.category.destroy',
+                                    'type' => 'route',
+                                    'confirm' => 'Are you sure you want to delete this news category?</br>This cannot be undone.',
+                                    'class' => 'item',
+                                    'prepend' => '<i class="delete icon"></i>',
+                                    'model' => $category,
+                                ])
+                            </div>
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="3">
+                            <div class="ui centered">{{ ___('There are no results') }}</div>
+                        </td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
 
-{{ $categories->links('pagination.default') }}
+    {{ $categories->links('pagination.default') }}
 
-@endsection
+    @endsection
+@else
+    @section('outer-content')
+        <div class="ui middle aligned padded grid no-result">
+            <div class="column">
+                <div class="ui center aligned padded grid">
+                    <div class="column">
+                        <h2>{{ ___('We couldn\'t find anything') }}</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endsection
+@endif
--- views/news/index.blade.php
+++ views/news/index.blade.php
@@ -16,48 +16,62 @@
     </div>
 @endsection
 
-@section('content')
-    <table class="ui selectable very basic table">
-        <thead>
-            <tr>
-                <th class="nine wide">{{ ___('News Title') }}</th>
-                <th>{{ ___('News Date') }}</th>
-                <th class="center aligned collapsing">{{ ___('Actions') }}</th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse($news as $item)
-                <tr class="nine wide" data-news-id="{{ $item->id }}">
-                    <td>{{ str_limit($item->title, 100) }}</td>
-                    <td>{{ (new Carbon\Carbon($item->getMeta('news_date')))->formatLocalized(___('%B %e, %Y')) }}</td>
-                    <td class="right aligned collapsing">
-                        <div class="ui compact text menu">
-
-                            <a class="item" href="{{ route('news.edit', $item->id) }}">
-                                <i class="pencil icon"></i>
-                                {{-- {{ ___('Edit') }} --}}
-                            </a>
-                            @button('', [
-                                'method' => 'delete',
-                                'location' => 'news.destroy',
-                                'type' => 'route',
-                                'confirm' => 'Are you sure you want to delete this news?</br>This cannot be undone.',
-                                'class' => 'item action',
-                                'prepend' => '<i class="delete icon"></i>',
-                                'model' => $item,
-                            ])
-                        </div>
-                    </td>
-                </tr>
-            @empty
+@if(count($news))
+    @section('content')
+        <table class="ui selectable very basic table">
+            <thead>
                 <tr>
-                    <td colspan="3">
-                        <div class="ui centered">{{ ___('There are no results') }}</div>
-                    </td>
+                    <th class="nine wide">{{ ___('News Title') }}</th>
+                    <th>{{ ___('News Date') }}</th>
+                    <th class="center aligned collapsing">{{ ___('Actions') }}</th>
                 </tr>
-            @endforelse
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @forelse($news as $item)
+                    <tr class="nine wide" data-news-id="{{ $item->id }}">
+                        <td>{{ str_limit($item->title, 100) }}</td>
+                        <td>{{ (new Carbon\Carbon($item->getMeta('news_date')))->formatLocalized(___('%B %e, %Y')) }}</td>
+                        <td class="right aligned collapsing">
+                            <div class="ui compact text menu">
 
-    {{ $news->links('pagination.default') }}
-@endsection
+                                <a class="item" href="{{ route('news.edit', $item->id) }}">
+                                    <i class="pencil icon"></i>
+                                    {{-- {{ ___('Edit') }} --}}
+                                </a>
+                                @button('', [
+                                    'method' => 'delete',
+                                    'location' => 'news.destroy',
+                                    'type' => 'route',
+                                    'confirm' => 'Are you sure you want to delete this news?</br>This cannot be undone.',
+                                    'class' => 'item action',
+                                    'prepend' => '<i class="delete icon"></i>',
+                                    'model' => $item,
+                                ])
+                            </div>
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="3">
+                            <div class="ui centered">{{ ___('There are no results') }}</div>
+                        </td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+
+        {{ $news->links('pagination.default') }}
+    @endsection
+@else
+    @section('outer-content')
+        <div class="ui middle aligned padded grid no-result">
+            <div class="column">
+                <div class="ui center aligned padded grid">
+                    <div class="column">
+                        <h2>{{ ___('We couldn\'t find anything') }}</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endsection
+@endif
